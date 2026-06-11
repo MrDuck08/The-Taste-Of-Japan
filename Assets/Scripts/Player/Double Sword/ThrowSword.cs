802730abdac75efbfa979d759309a3c465f6914a
Alex Nilsson <145462017+MrDuck08@users.noreply.github.com>
2026-06-11 23:01:51 +0200

    Trying To Rotate Sword,

--- Assets/Scripts/Player/Double Sword/ThrowSword.cs
+++ Assets/Scripts/Player/Double Sword/ThrowSword.cs
@@ -1,4 +1,5 @@
 using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ThrowSword : MonoBehaviour
@@ -6,10 +7,13 @@ public class ThrowSword : MonoBehaviour
     Rigidbody2D myRigidbody2D;
 
     bool hitSomething = false;
+    bool hitEnemy = false;
 
     [SerializeField] float speed = 50;
     [SerializeField] float distanceAfterHit = 0.4f;
     [SerializeField] LayerMask ignoreMask;
+
+    GameObject collisionObject = null;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -35,11 +39,49 @@ public class ThrowSword : MonoBehaviour
             {
                 hitSomething = true;
                 myRigidbody2D.linearVelocity = Vector2.zero;
+
+                collisionObject = hit.transform.gameObject;
+                if (collisionObject.tag == "Enemy")
+                {
+                    hitEnemy = true;
+                }
+                else
+                {
+                    float _whatSideOfObj = 1;
+
+                    if(Mathf.Abs(hit.point.y - collisionObject.transform.position.y) > Mathf.Abs(hit.point.x - collisionObject.transform.position.x))
+                    {
+                        _whatSideOfObj = -1;
+                        Debug.Log("Closer To X");
+                        
+                    }
+
+                    Debug.Log("X Hit Pos: " + hit.point.x);
+                    Debug.Log("Obj X + Scale: " + (Mathf.Abs(collisionObject.transform.position.x) + collisionObject.transform.localScale.y / 2));
+                    float distanceToXWall = Mathf.Abs(hit.point.x - collisionObject.transform.position.x) + collisionObject.transform.localScale.x/2;
+                    Debug.Log(Mathf.Abs(distanceToXWall));
+
+
+                    float angle = Mathf.Atan2((hit.point.y - transform.position.y) * _whatSideOfObj, (hit.point.x - transform.position.x) * _whatSideOfObj) * Mathf.Rad2Deg;
+
+                    //float angle = Mathf.Atan2(transform.position.y - hit.point.y, transform.position.x - hit.point.x) * Mathf.Rad2Deg;
+                    //transform.rotation = Quaternion.Euler(0, 0, -angle - 90);
+                    //transform.localEulerAngles *= -1;
+                    transform.localEulerAngles = new Vector3(0, 0, -angle - 90);
+
+                }
             }
 
         }
 
         #endregion
+
+        if(hitSomething && !hitEnemy)
+        {
+
+
+
+        }
 
     }
 
