1d08ba715a6999e66a464b5e8395ee7cf3ee29da
Alex Nilsson <145462017+MrDuck08@users.noreply.github.com>
2026-06-12 11:34:41 +0200

    Sword can now bounde, the solution was way simpler

--- Assets/Scripts/Player/Double Sword/ThrowSword.cs
+++ Assets/Scripts/Player/Double Sword/ThrowSword.cs
@@ -29,7 +29,9 @@ public class ThrowSword : MonoBehaviour
         if (!hitSomething)
         {
 
+            
             RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.up, 1337, ~ignoreMask);
+            
 
             transform.position = Vector2.MoveTowards(transform.position, hit.point, speed * Time.deltaTime);
 
@@ -47,28 +49,15 @@ public class ThrowSword : MonoBehaviour
                 }
                 else
                 {
-                    float _whatSideOfObj = 1;
+                    
+                    Vector2 reflectDir = Vector2.Reflect(transform.up, hit.normal);
 
-                    if(Mathf.Abs(hit.point.y - collisionObject.transform.position.y) > Mathf.Abs(hit.point.x - collisionObject.transform.position.x))
-                    {
-                        _whatSideOfObj = -1;
-                        Debug.Log("Closer To X");
-                        
-                    }
+                    // Förvandlar riktningen till grader
+                    float angle = Mathf.Atan2(reflectDir.y, reflectDir.x) * Mathf.Rad2Deg;
 
-                    // Väggar är roterade så man kan inte ta scala x som exempel
-                    Debug.Log("X Hit Pos: " + hit.point.x);
-                    Debug.Log("Obj X + Scale: " + (Mathf.Abs(collisionObject.transform.position.x) + collisionObject.transform.localScale.y / 2));
-                    float distanceToXWall = Mathf.Abs(hit.point.x - collisionObject.transform.position.x) + collisionObject.transform.localScale.x/2;
-                    Debug.Log(Mathf.Abs(distanceToXWall));
+                    transform.rotation = Quaternion.Euler(0, 0, angle - 90f);
 
-
-                    float angle = Mathf.Atan2((hit.point.y - transform.position.y) * _whatSideOfObj, (hit.point.x - transform.position.x) * _whatSideOfObj) * Mathf.Rad2Deg;
-
-                    //float angle = Mathf.Atan2(transform.position.y - hit.point.y, transform.position.x - hit.point.x) * Mathf.Rad2Deg;
-                    //transform.rotation = Quaternion.Euler(0, 0, -angle - 90);
-                    //transform.localEulerAngles *= -1;
-                    transform.localEulerAngles = new Vector3(0, 0, -angle - 90);
+                    hitSomething = false;
 
                 }
             }
